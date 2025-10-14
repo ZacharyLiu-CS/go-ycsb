330a3b1875684c1b09795238e4f1c7f6114cd560
zacharyzliu <zacharyzliu@tencent.com>
2025-10-14 16:03:11 +0800

    Add walfifo benchmark workload template and scripts

---
+++ scripts/wal_bench_run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# SPDX-License-Identifier: Apache-2.0
+echo "./bin/go-ycsb  run tikv -p tikv.pd="127.0.0.1:42379" -p tikv.type="raw" -p tikv.column_family="walfifo" -P workloads/workload_walfifo_test -p threadcount=256"
+./bin/go-ycsb  run tikv -p tikv.pd="127.0.0.1:42379" -p tikv.type="raw" -p tikv.column_family="walfifo" -P workloads/workload_walfifo_test -p threadcount=256
+echo "./bin/go-ycsb  run tikv -p tikv.pd="127.0.0.1:42379" -p tikv.type="raw" -p tikv.column_family="default" -P workloads/workload_walfifo_test -p threadcount=256"
+./bin/go-ycsb  run tikv -p tikv.pd="127.0.0.1:42379" -p tikv.type="raw" -p tikv.column_family="default" -P workloads/workload_walfifo_test -p threadcount=256
